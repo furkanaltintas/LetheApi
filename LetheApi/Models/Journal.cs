d286fcf1e36ed46f2b0472c3f90fa59733faa901
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-06-13 23:22:27 +0300

    Slug eklendi

--- LetheApi/Models/Journal.cs
+++ LetheApi/Models/Journal.cs
@@ -4,6 +4,7 @@ public class Journal
 {
     public int Id { get; set; }
     public string CategoryName { get; set; } = string.Empty;
+    public string Slug { get; set; } = string.Empty;
     public string Title { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
 }
