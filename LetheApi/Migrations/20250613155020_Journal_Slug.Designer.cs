92cde4e6ed9e06d5e2dbea625bc3b09b374daa57
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-06-13 23:22:07 +0300

    mig

--- LetheApi/Migrations/20250613155020_Journal_Slug.Designer.cs
+++ LetheApi/Migrations/20250613155020_Journal_Slug.Designer.cs
@@ -3,6 +3,7 @@ using LetheApi.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LetheApi.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250613155020_Journal_Slug")]
+    partial class Journal_Slug
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ LetheApi/Migrations/20250613155020_Journal_Slug.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LetheApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class Journal_Slug : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Slug",
+                table: "Journals",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.UpdateData(
+                table: "Journals",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Slug",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Journals",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Slug",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Journals",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Slug",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Journals",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Slug",
+                value: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Slug",
+                table: "Journals");
+        }
+    }
+}
