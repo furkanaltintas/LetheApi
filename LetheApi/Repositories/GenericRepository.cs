a875ae1b746dc6ca044619f4edef06a0450bd81a
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-06-13 23:22:37 +0300

    generic repository

---
+++ LetheApi/Repositories/GenericRepository.cs
@@ -0,0 +1,72 @@
+﻿using LetheApi.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace LetheApi.Repositories;
+
+public class GenericRepository<T> : IGenericRepository<T> where T : class, new()
+{
+    private readonly AppDbContext _context;
+    private readonly DbSet<T> _dbSet;
+
+    public GenericRepository(AppDbContext context)
+    {
+        _context = context;
+        _dbSet = _context.Set<T>();
+    }
+
+    public async Task AddAsync(T entity)
+    {
+        await _dbSet.AddAsync(entity);
+    }
+
+    public void Delete(T entity)
+    {
+        _dbSet.Remove(entity);
+    }
+
+    public async Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate)
+    {
+        return await _dbSet.Where(predicate).ToListAsync();
+    }
+
+    public async Task<IEnumerable<T>> GetAllAsync()
+    {
+        return await _dbSet.ToListAsync();
+    }
+
+    public async Task<T?> GetByAsync(Expression<Func<T, bool>> predicate)
+    {
+        return await _dbSet.Where(predicate).FirstOrDefaultAsync();
+    }
+
+    public async Task<T?> GetByIdAsync(int id)
+    {
+        return await _dbSet.FindAsync(id);
+    }
+
+    public async Task<T?> GetFirstAsync()
+    {
+        return await _dbSet.FirstOrDefaultAsync();
+    }
+
+    public IQueryable<T> Query()
+    {
+        return _dbSet.AsQueryable();
+    }
+
+    public int SaveChanges()
+    {
+        return _context.SaveChanges();
+    }
+
+    public async Task<int> SaveChangesAsync()
+    {
+        return await _context.SaveChangesAsync();
+    }
+
+    public void Update(T entity)
+    {
+        _dbSet.Update(entity);
+    }
+}
---
+++ LetheApi/Repositories/IGenericRepository.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq.Expressions;
+
+namespace LetheApi.Repositories;
+
+public interface IGenericRepository<T> where T : class, new()
+{
+    IQueryable<T> Query();
+
+    Task<IEnumerable<T>> GetAllAsync();
+    Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate);
+
+    Task<T?> GetFirstAsync();
+    Task<T?> GetByIdAsync(int id);
+    Task<T?> GetByAsync(Expression<Func<T, bool>> predicate);
+
+    Task AddAsync(T entity);
+    void Update(T entity);
+    void Delete(T entity);
+
+    Task<int> SaveChangesAsync();
+    int SaveChanges();
+}
