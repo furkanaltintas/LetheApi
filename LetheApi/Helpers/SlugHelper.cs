619f8930af134073809ee14aff02224d65a25a8c
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-06-13 23:22:02 +0300

    Slug

---
+++ LetheApi/Helpers/SlugHelper.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+
+namespace LetheApi.Helpers;
+
+public static class SlugHelper
+{
+    public static String GenerateSlug(string text)
+    {
+        text = text.ToLowerInvariant();
+        text = Regex.Replace(text, @"[^a-z0-9\s-]", ""); // özel karakterleri kaldırır
+        text = Regex.Replace(text, @"\s+", " ").Trim(); // fazla boşlukları azaltır
+        text = Regex.Replace(text, @"\s", "-"); // boşlukları tireye çevir
+        return text;
+    }
+}
